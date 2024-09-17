04d6174fb3343a1753f1562282409a096d0c72ba
mthrsc <mthrsc@me.com>
2024-09-17 21:26:45 +0100

    Remove comments

--- project.py
+++ project.py
@@ -24,23 +24,3 @@ if __name__ == "__main__":
         except SystemExit:
             os._exit(130)
 
-
-
-# def thread_example():
-#     t1 = threading.Thread(target=t1_func, args=(0,), name="thread1")
-#     t2 = threading.Thread(target=t2_func, args=(0,))
-
-#     t1.start()
-#     t2.start()
-
-
-# def t1_func(i):
-#     while(i < 10):
-#         i = i + 1
-#         print("i: ", i)
-#     print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
-
-# def t2_func(y):
-#     while(y < 20):
-#         y = y + 2
-#         print("y: ", y)    
