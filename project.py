15218d2df45b2a6944d5cb036cbaa37265799f0a
mthrsc <mthrsc@me.com>
2024-10-22 11:01:12 +0100

    Comments

--- project.py
+++ project.py
@@ -4,19 +4,23 @@ import os
 
 def main():
     app = tkinterApp.tkinterApp()
+    
     set_window_size(app, "1050x600")
     set_window_name(app, "PCS - Pokemon Card Scanner")
     set_close_cross_behaviour(app)
     
     app.mainloop()
 
+# Three custom function in project.py requested by assignment
+# Set app title
 def set_window_name(app, title):
-    # Set window title
     app.title(title)
 
+# Set app size on screen
 def set_window_size(app, resolution):
     app.geometry(resolution)
 
+# Set what happens when clicking the closing cross
 def set_close_cross_behaviour(app):
     app.protocol("WM_DELETE_WINDOW", sys.exit)
 
